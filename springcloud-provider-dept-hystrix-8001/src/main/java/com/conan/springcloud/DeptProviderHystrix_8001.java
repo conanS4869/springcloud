3d0fced8fb2dd5862145301469d459ab7bb169e1
conanS4869 <87404816@qq.com>
2020-09-04 17:17:23 +0800

    update

--- springcloud-provider-dept-hystrix-8001/src/main/java/com/conan/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/conan/springcloud/DeptProviderHystrix_8001.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +13,11 @@ import org.springframework.context.annotation.Bean;
 @MapperScan("com.conan.springcloud.dao")
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableCircuitBreaker  // 添加对熔断的支持
 @SpringBootApplication
-public class DeptProvider_8001 {
+public class DeptProviderHystrix_8001 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider_8001.class, args);
+        SpringApplication.run(DeptProviderHystrix_8001.class,args);
     }
 
     @Bean
---
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/conan/springcloud/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.conan.springcloud.service;
+
+
+import com.conan.springcloud.pojo.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    boolean addDept(Dept dept);
+
+    Dept queryById(Long id);
+
+    List<Dept> queryAll();
+}
